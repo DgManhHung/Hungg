0c55ca75bfe8d53d23032eb316f80b0bd7d89e30
Dương Mạnh Hùng <manhhungtncntt@gmail.com>
2025-09-10 05:38:38 +0700

    Add files via upload

---
+++ 0101.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int m;
+    std::cin >> m;
+    while(m--){
+    long long a,b=0;
+    std::cin >>  a ;
+    b=(a+1)*a/2;
+    std::cout << b << std::endl;
+}
+}
